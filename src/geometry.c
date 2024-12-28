9b181a399b7695fddec67bb847a3e82a27debea9
RobinSPET <robin57spet@gmail.com>
2024-12-28 21:53:43 +0100

    ex23

--- src/geometry.c
+++ src/geometry.c
@@ -111,7 +111,9 @@ void print_segment(const void * s) {
 int point_precedes(const void * p1, const void * p2) {
 	assert(p1);
 	assert(p2);
-	// TODO
+
+	if (lt(get_x((p1)), get_x(p2)) || (eq(get_x(p1), get_x(p2)) && gt(get_y(p1), get_y(p2)))) return 1;
+	return 0;
 }
 
 int segment_precedes(const struct Segment * s1, const struct Segment * s2, struct Rational x0) {
